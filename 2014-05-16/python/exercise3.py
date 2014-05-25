2c31ea2c8d3ed288181dfa9b5b5e9740f25552d0
Davide <bernardini.davide@yahoo.it>
2014-05-25 03:34:24 +0200

    homework3

--- 2014-05-16/python/exercise3.py
+++ 2014-05-16/python/exercise3.py
@@ -19,17 +19,16 @@ from mapper import *
 from boolean import *
 
 
-"""metodo enumerazione"""
-#cellNumbering (larModel,hpcModel)
 
-"""metodo fusione"""
-#diagram2cell(diagram,master,cell)
-
-"""metodo eliminazione"""
-#V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
 
 
 """creo una funzione che applica il ciclo fusione-enumerazione-fusione"""
+
+"""QUESTO ESERCIZIO E DIVISO IN 4 PARTI: UNA PARTE  COMPRENDe FUNZIONI CHE SEMPLIFICANO TANTE OPERAZIONI ELEMENTARI (RIMUOVI, FONDI, ENUMERA CELLE ECC..) 
+   IN POCHE ISTRUZIONI; UN ALTRA PARTE COMPRENDE FUNZIONI CHE  AUTOMATIZZA OPERAZIONI ELEMENTARI COME MERGE ED ELIMINATE SENZA DOVER RIGUARDARE LE
+   CELLE NUMERATE; UN ALTRA PARTE COMPRENDE FUNZIONI CHE CONSENTONO DI UNIRE CELLE DI UN DIAGRAMMA; L ULTIMA PARTE INVECE E DEDICATA AI TEST:
+   CE NE SONO DUE: IL PRIMO USA FUNZIONI ELEMENTARI SEMPLIFICATI CON LA NECESSITA PERO DI GUARDARE LE CELLE NUMERATE; IL SECONDO USA UNA FUNZIONE
+   (POSSIAMO DIRE, QUELLA PIU  RICHIESTA IN QUESTO ESERCIZIO) CHE ELIMINA E FONDE DIRETTAMENTE SENZA DOVER RIGUARDARE LE CELLE NUMERATE  """
 
 
 #funzione di appoggio che mi consente di restituire il diagramma-modello con le celle numerate
@@ -42,7 +41,7 @@ def toNumber_faces(model):
     
 
 """metodo PRONCIPALE che mi permette di INSERIRE IL SUB-DIAGRAMMA IN DIVERSE CELLE DEL MASTER, obg=SUB-DIAGRAM  model=MASTER  
-    lista=LISTA CELLE DEL MASTER SULLE QUALI VOGLIO INSERIRE IL SUBDIAGRAM"""
+    lista=LISTA CELLE DEL MASTER SULLE QUALI VOGLIO INSERIRE IL SUBDIAGRAM, includendo anche la visione dell'output numerato"""
 def toReplicate_into_masters2cell(obg,model,lista):
     lista.sort();
     lista.reverse();
@@ -52,7 +51,7 @@ def toReplicate_into_masters2cell(obg,model,lista):
     return model
 
 
-#funzione che mi rimuove determinate celle di una diagramma
+#funzione che mi rimuove determinate celle di una diagramma, includendo anche la visione dell'output numerato
 def toRemoveCells(model,lista):
     V,CV = model
     model =  V,[cell for k,cell in enumerate(CV) if not (k in lista)]
@@ -60,6 +59,38 @@ def toRemoveCells(model,lista):
     return model
 
 
+
+
+"""ALTRA FUNZIONE PRINCIPALE: CREO UNA FUNZIONE PIU COMPLESSA CHE CONTEMPORANEAMENTE FA IL REMOVE E IL MERGE CALCOLANDOSI IL CAMBIAMENTO DELL ENUMERAZIONI 
+   DELLE CELLE SENZA A STARE A RIGUARDARLI OGNI VOLTA """
+   
+
+#funzione di appoggio che mi decrementa el di una quantita pari al numero degli elementi della lista per i quali el is maggiore 
+def decr_qt(el,lista):
+    cont = 0
+    for k in lista:
+        if k<el:
+            cont+=1
+    return el-cont
+
+#funzione di appoggio che usa la funzione di sopra (decr_qt) per applicarla a tutti gli elementi della lista del primo parametro in base all altra
+#lista passata come secondo parametro
+def decr_qt_all(lista,ll):
+    return [decr_qt(k,ll) for k in lista]
+
+
+#FUNZIONE PRINCIPALE CHE AUTOMATIZZA TUTTO
+def cycle_merge_eliminate(diagram,master,toRemove,toMerge):
+    toMerge = decr_qt_all(toMerge,toRemove)
+    V,CV = master
+    master =  V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
+    toMerge.sort()
+    toMerge.reverse()
+    for k in toMerge:
+        master = diagram2cell(diagram,master,k)
+    toNumber_faces(master)
+    return master
+    
 
 
     
@@ -127,6 +158,7 @@ master = toRemoveCells(master,[13,33,17,37])
 
 #inseriamo il diagramma per la porta
 diagram = assemblyDiagramInit([3,1,2])([[2,1,2],[.3],[2.2,.5]])
+toNumber_faces(diagram)
 
 #adesso inseriamo il diagram su diverse celle del master
 master = toReplicate_into_masters2cell(diagram,master,[29,32,14])
@@ -143,4 +175,26 @@ toNumber_faces(master)
 master = toRemoveCells(toReplicate_into_masters2cell(diagram,master,[30]),[57])
 
 VIEW(STRUCT(MKPOLS(master)))
+
+
+""" * * * TEST2 * * * """
+"""Rifacciamo le stesse operazioni usando pero l altra funzione principale complessa che automatizza il ciclo (cycle_merge_eliminate)"""
+
+#ridefinisco il solito master
+master = assemblyDiagramInit([5,5,2])([[.3,3.2,.1,5,.3],[.3,4,.1,2.9,.3],[.3,2.7]])
+V,CV = master
+toNumber_faces(master)
+
+#diagramma porta
+diagram = assemblyDiagramInit([3,1,2])([[2,1,2],[.3],[2.2,.5]])
+diagram = toRemoveCells(diagram,[2])
+
+#ora applichiamo la funzione PRINCIPALE cycle_merge_eliminate in modo che con una funzione elimino e inserisco direttamente le porte
+#senza a stare a riguardare nuovamente i  numeri
+toRemove=[13,17,33,37]
+toMerge=[31,35,15]
+master = cycle_merge_eliminate(diagram,master,toRemove,toMerge)
+
+VIEW(STRUCT(MKPOLS(master)))
+
     
